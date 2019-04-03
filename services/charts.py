499d62e385e58d4840a35a91416b57eef284a75e
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-04-03 22:41:51 +0100

    Refactor services

---
+++ services/charts.py
@@ -0,0 +1,169 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+import pandas as pd
+
+FIGURE_DPI = 72
+
+sns.set(style='whitegrid')
+
+
+def makeup_chart(fontsize, value_margin, is_percentage, show_integer):
+    if is_percentage:
+        percentage_symbol = '%'
+    else:
+        percentage_symbol = ''
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize * 0.75)
+    ax = plt.gca()
+    if show_integer:
+        annotation = '{:.0f}{}'
+    else:
+        annotation = '{:.2f}{}'
+    # show values in bars
+    for p in ax.patches:
+        ax.annotate(
+            annotation.format(p.get_height(), percentage_symbol),
+            (p.get_x() + p.get_width() / 2., p.get_height() - value_margin),
+            ha='center', va='center', fontsize=fontsize, color='white'
+        )
+    # hide right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+
+def bar_chart(serie, is_percentage=True, show_integer=True):
+    FONTSIZE = 25
+    serie = serie.sort_values()
+    plt.figure(figsize=(30, 10), dpi=FIGURE_DPI)
+    sns.barplot(serie.index, serie)
+    makeup_chart(FONTSIZE, serie.max() / 20, is_percentage, show_integer)
+
+
+def factor_chart(data,
+                 y,
+                 value_margin=2,
+                 is_percentage=True,
+                 estimator=np.mean,
+                 show_integer=True):
+    FONTSIZE = 14
+    plt.figure(figsize=(30, 10), dpi=FIGURE_DPI)
+    colors = ['orange red', 'windows blue', 'amber']
+    custom_palette = sns.xkcd_palette(colors)
+    sns.catplot(
+        x='etapa',
+        y=y,
+        hue='curso',
+        height=7,
+        aspect=2,
+        legend=False,
+        estimator=estimator,
+        data=data.reset_index(),
+        kind='bar',
+        palette=custom_palette,
+        ci=None)
+    plt.legend(
+        loc='best', prop={'size': FONTSIZE}, frameon=True, framealpha=0.9)
+    makeup_chart(FONTSIZE, value_margin, is_percentage, show_integer)
+
+
+def color_gradient(serie, palette_name, inverse=False):
+    unique_values = np.sort(serie.unique())
+    if inverse:
+        unique_values = unique_values[::-1]
+    pal = sns.color_palette(palette_name, unique_values.size)
+    cmap = {v: c for v, c in zip(unique_values, pal)}
+    return np.array(serie.map(lambda x: cmap[x]))
+
+
+def delta_chart(*args, label, inverse=False):
+    CONFIG = ({
+        'color': 'Greens',
+        'verb': 'mejorado'
+    }, {
+        'color': 'Reds_r',
+        'verb': 'empeorado'
+    })
+
+    fig, axes = plt.subplots(1, 2, figsize=(15, 5), dpi=FIGURE_DPI)
+    for ix, ax in enumerate(axes):
+        serie = args[ix]['delta']
+        if len(serie) == 0:
+            continue
+        sns.barplot(
+            serie.index,
+            serie,
+            ax=axes[ix],
+            palette=color_gradient(serie, CONFIG[ix]['color'], inverse))
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='-', alpha=0.5)
+        verb = CONFIG[ix]['verb']
+        title = f'Grupos que han {verb} en {label}'
+        ax.set_title(title)
+
+
+def stacked_chart(data, y, x1, x2, x1_label=None, x2_label=None):
+    f, ax = plt.subplots(figsize=(6, 15), dpi=FIGURE_DPI)
+
+    sns.set_color_codes('pastel')
+    if not x1_label:
+        x1_label = x1
+    sns.barplot(x=x1, y=y, data=data, label=x1_label, color='b')
+
+    sns.set_color_codes('muted')
+    if not x2_label:
+        x2_label = x2
+    sns.barplot(x=x2, y=y, data=data, label=x2_label, color='b')
+
+    ax.legend(ncol=1, loc='upper right', frameon=True)
+    ax.set(ylabel='', xlabel='')
+
+    sns.despine()
+
+
+def rgb_heatmap(data):
+    cdict = {
+        'red': ((0.0, 1.0, 0.7), (0.5, 1.0, 0.7), (1.0, 0.0, 0.0)),
+        'green': ((0.0, 0.0, 0.0), (0.5, 1.0, 0.4), (1.0, 1.0, 0.4)),
+        'blue': ((0.0, 0.0, 0.0), (1.0, 0.0, 0.0))
+    }
+
+    plt.figure(figsize=(5, 5), dpi=FIGURE_DPI * 2)
+    cmap = LinearSegmentedColormap('custom_cmap', cdict, 4)
+    sns.heatmap(data, annot=True, cmap=cmap, center=5)
+    # hide axis labels
+    plt.xlabel('')
+    plt.ylabel('')
+    # hide ticks
+    plt.tick_params(top=False, bottom=False, left=False,
+                    right=False, labelleft=True, labelbottom=True)
+
+
+def magic_groups(data):
+    fig, (ax1, ax2) = plt.subplots(
+        1, 2, figsize=(15, 5), sharey=True, dpi=2 * FIGURE_DPI)
+
+    best_magic = pd.melt(
+        data.head(5).reset_index(),
+        id_vars='grupo',
+        value_vars=['éxito', 'absentismo', 'partes'])
+    g = sns.catplot(x='grupo', y='value', hue='variable',
+                    data=best_magic, kind='bar', ax=ax1)
+    ax1.set_title('Mejores grupos mágicos')
+    ax1.set(ylabel='', xlabel='')
+    ax1.legend(loc='best', frameon=True, framealpha=0.9)
+    plt.close(g.fig)
+
+    worse_magic = pd.melt(
+        data.tail(5).reset_index(),
+        id_vars='grupo',
+        value_vars=['éxito', 'absentismo', 'partes'])
+    g = sns.catplot(x='grupo', y='value', hue='variable',
+                    data=worse_magic, kind='bar', ax=ax2, legend_out=True)
+    ax2.set_title('Peores grupos mágicos')
+    ax2.set(ylabel='', xlabel='')
+    ax2.legend(loc='best', frameon=True, framealpha=0.9)
+    plt.close(g.fig)
---
+++ services/loaders.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import os
+
+COMPETENCE_CRITERIA = {
+    'PA-%': 'PA',
+    'AD-%': 'AD',
+    'MA-%': 'MA',
+    'EX-%': 'EX'
+}
+
+STUDIES = {
+    '1º Educación Secundaria Obligatoria (LOMCE)': '1ESO',
+    '2º Educación Secundaria Obligatoria (LOMCE)': '2ESO',
+    '3º Educación Secundaria Obligatoria (LOMCE)': '3ESO',
+    '4º Educación Secundaria Obligatoria (LOMCE)': '4ESO',
+    ('Primer curso del Programa de Mejora del Aprendizaje y el '
+        'Rendimiento (LOMCE)'): '1PMAR',
+    ('Segundo curso del Programa de Mejora del Aprendizaje y el '
+        'Rendimiento (LOMCE)'): '2PMAR'
+}
+
+COMPETENCE_ITEMS = {
+    'Comunicación lingüística': 'CL',
+    ('Competencia matemática y competencias básicas en '
+        'ciencia y tecnología'): 'CMCT',
+    'Competencia digital': 'CD',
+    'Aprender a aprender': 'AAP',
+    'Competencias sociales y cívicas': 'CSC',
+    'Sentido de iniciativa y espíritu emprendedor': 'SIEE',
+    'Conciencia y expresiones culturales': 'CEC'
+}
+
+COMPETENCE_WEIGHTS = {
+    'PA': 0.025,
+    'AD': 0.050,
+    'MA': 0.075,
+    'EX': 0.1
+}
+
+COLUMNS = {
+    'GRUPO': 'grupo',
+    'ESTUDIO': 'nivel',
+    'ITEM': 'item',
+}
+
+
+def load_bc(path):
+    df = pd.read_csv(
+        path,
+        encoding='cp1252',
+        sep=';',
+        skipfooter=9,
+        engine='python')
+
+    df = df[df['GRUPO'] != 'TOTAL ESTUDIO']
+
+    df = df.rename(columns=COMPETENCE_CRITERIA)
+    df = df.rename(columns=COLUMNS)
+
+    df = df[['nivel', 'grupo', 'item', *COMPETENCE_CRITERIA.values()]]
+    df['nivel'].replace(STUDIES, inplace=True)
+    df['item'].replace(COMPETENCE_ITEMS, inplace=True)
+
+    # summary value for each group-item
+    df['marca'] = COMPETENCE_WEIGHTS['PA'] * df['PA'] + \
+        COMPETENCE_WEIGHTS['AD'] * df['AD'] + \
+        COMPETENCE_WEIGHTS['MA'] * df['MA'] + \
+        COMPETENCE_WEIGHTS['EX'] * df['EX']
+
+    return df
+
+
+def load_data(year, evaluation):
+    df = pd.DataFrame()
+    df_bc = pd.DataFrame()
+    labels = []
+    for y in range(year - 2, year + 1):
+        year_label = 'C' + str(y)[2:] + str(y + 1)[2:]
+        path = os.path.join('../data', year_label + '.xlsx')
+        if y == year:
+            num_evaluations = evaluation
+        else:
+            num_evaluations = 3
+        for ev in range(1, num_evaluations + 1):
+            evaluation_label = f'E{ev}'
+            partial_df = pd.read_excel(path, sheet_name=evaluation_label)
+            partial_df['curso'] = year_label
+            partial_df['evaluación'] = evaluation_label
+            partial_df['absentismo'] = partial_df[
+                'absentismo_justificado'] + partial_df[
+                    'absentismo_injustificado']
+            partial_df['éxito_abs'] = round(
+                partial_df['ratio'] * (partial_df['éxito'] / 100))
+            partial_df.set_index(['curso', 'evaluación', 'grupo'],
+                                 inplace=True)
+
+            # Loading basic competences
+            partial_df_bc = load_bc('../data/ccbb/C1819E2_ESO_CCBB.csv')
+            partial_df_bc['curso'] = year_label
+            partial_df_bc['evaluación'] = evaluation_label
+            partial_df_bc.set_index(['curso', 'evaluación', 'grupo'],
+                                    inplace=True)
+
+            # grouping basic competences by groups (summary value)
+            partial_df_bc_grouped = partial_df_bc.groupby(
+                'grupo').mean()['marca']
+            partial_df = pd.merge(
+                partial_df,
+                pd.DataFrame(partial_df_bc_grouped),
+                left_index=True,
+                right_index=True,
+                how='outer',
+                sort=False).rename(columns={'marca': 'ccbb'})
+            partial_df_bc.index.name = 'grupo'
+
+            df = df.append(partial_df)
+            df_bc = df_bc.append(partial_df_bc)
+
+            labels.append((year_label, evaluation_label))
+
+    df.fillna(0, inplace=True)
+    df_bc.fillna(0, inplace=True)
+
+    return df, df_bc, labels
+
+
+def get_data_by_stages(df, stages):
+    if not isinstance(stages, list):
+        stages = [stages]
+    return df[df['etapa'].isin(stages)]
